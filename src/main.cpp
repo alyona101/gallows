068d50635244767adf2cc1cd6aad2349d25eadff
Viktor080181 <161635347+Viktor080181@users.noreply.github.com>
2024-05-01 20:04:58 +0300

    Мой первый коммит (#12) (#17)
    * Мой первый коммит (#12)
    
    * viktor
    
    ---------
    
    Co-authored-by: alyona101 <159888073+alyona101@users.noreply.github.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <string>
+#include <Windows.h> 
+
+using namespace std;
+
+
+template <typename Type>
+class
+{
+    struct
+    {
+
+    }
+public:
+
+
+private:
+
+};
 
 int main()
 {
-    std::cout << "H2324i" << std::endl;
-    system("pause");
+    setlocale(LC_ALL, "Russian");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+
 }
